bda099f7dbccfbe25a6e32ca3e70a78d18b85e5f
Sushant Bansal <1star.sushant@gmail.com>
2017-11-22 16:39:45 +0530

    Added MVC Structure

---
+++ config/express.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+module.exports = function() {
+    var app = express();
+    require('../app/routes/index.server.routes.js')(app);
+    app.set('views', './app/views');
+	app.set('view engine', 'ejs');
+	app.use(express.static('./public'));
+    return app;
+};
--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'));
-
-app.set('view engine', 'ejs')
-
-app.get('/', function (req, res) {
-  res.render('index');
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})	
+var port = 1337;
+var express = require('./config/express');
+var app = express();
+app.listen(port);
+module.exports = app;
+console.log('Server running at http://localhost:' + port);
