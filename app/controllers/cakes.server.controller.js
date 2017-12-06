86f1919fafd7940accb87d992924d224f0b9d810
jj116 <jaison.basket@gmail.com>
2017-12-07 00:27:07 +0530

    Update

--- app/controllers/cakes.server.controller.js
+++ app/controllers/cakes.server.controller.js
@@ -1,6 +1,16 @@
+var Product = require('mongoose').model('Product');
+
 exports.render = function(req, res) {
-	    	res.render('cakes', {
+	Product.find({ "category": "Cakes"}, function(err, prods) {
+	    if (err) {
+	    	return next(err);
+	    }
+	    else{
+	    	res.render('pasteries', {
     		title: 'The Baking Room',
+    		list:prods,
     		user: req.user ? req.user.username : ''
     		});
+	    }
+	});
 };
--- app/controllers/pasteries.server.controller.js
+++ app/controllers/pasteries.server.controller.js
@@ -1,6 +1,16 @@
+var Product = require('mongoose').model('Product');
+
 exports.render = function(req, res) {
+	Product.find({ "category": "Pastry"}, function(err, prods) {
+	    if (err) {
+	    	return next(err);
+	    }
+	    else{
 	    	res.render('pasteries', {
     		title: 'The Baking Room',
+    		list:prods,
     		user: req.user ? req.user.username : ''
     		});
+	    }
+	});
 };
