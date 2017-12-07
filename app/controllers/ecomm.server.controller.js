40c71ec0716ac60746a8187c292b1a89d2145577
Sushant Bansal <1star.sushant@gmail.com>
2017-12-08 00:21:13 +0530

    Update

--- app/controllers/ecomm.server.controller.js
+++ app/controllers/ecomm.server.controller.js
@@ -1,4 +1,14 @@
 exports.render = function(req, res) {
+	var cartItems = req.cookies.CartCookie;
+	if (cartItems == undefined)
+	{
+		res.cookie('CartCookie',{items:[]});
+		// console.log('cookie created successfully');
+	} 
+	else
+	{	
+		console.log("Cookie Exists Already");
+	} 
 	var currCart = req.cookies.CartCookie; 
 	res.render('ecomm', {
 		title: 'The Baking Room',
